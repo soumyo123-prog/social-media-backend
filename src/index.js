4aa1a437d96111c9c1e09bc6c5859b19ee6aee8d
Your Name <you@example.com>
2021-06-02 21:39:01 +0530

    enabling cors

--- src/index.js
+++ src/index.js
@@ -9,6 +9,16 @@ const userRouter = require('./routers/user');
 const postRouter = require('./routers/post');
 
 app.use(express.json());
+
+// app.use((req,res,next) => {
+//     res.header('Access-Control-Allow-Origin','*');
+//     res.header('Access-Control-Allow-Headers','*');
+//     if (req.method === 'OPTIONS') {
+//         res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+//         return res.status(200).send({});
+//     }
+// })
+
 app.use(userRouter);
 app.use(postRouter);
 
